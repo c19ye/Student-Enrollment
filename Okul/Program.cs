5f982eb205e5ff9338ba48f81b789510d17f66d0
c19ye <80114964+c19ye@users.noreply.github.com>
2022-04-29 03:12:13 +0300

    Add files via upload

---
+++ Okul/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Okul
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                //başlangıçta default ayarlı bir sağlayıcısının olmasını istemedik bu yüzden false değerini verdik
+                .UseDefaultServiceProvider(options => options.ValidateScopes=false)
+        ;
+    }
+}
---
+++ Okul/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Okul.Data;
+
+namespace Okul
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            //servisin içine tanımladık
+            //adddbcontext diyerek veritabanıcontext sınıfını ve sqlite veritabanımızı eşleştiriyoruz
+            //connection stringimiz kolay bir şekilde tanımlanıyor
+            //databasenin ismini yazdık veri.db olarak ve projemizin içine entegre edecek kendi
+            services.AddDbContext<VeritabaniContext>(options => options.UseSqlite("Data source=veri.db"));
+            //session ekliyoruz ve bu sesionda 10 dakika sonra oturumu otomatik sonlandır diyorusz
+            services.AddSession(x => x.IdleTimeout = TimeSpan.FromMinutes(10));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseSession();//program başlatılınca sessionu kullan diyoruz
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            //yazarak program başlangıcında tohumlamayı yapmasını sağladık
+            SeedData.seed(app);
+        }
+    }
+}
